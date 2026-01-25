49cf38df322a99c006dd0e08434f6cd0aa1b5216
balakg <balakg@mit.edu>
2026-01-25 22:09:49 +0000

    Deploying to gh-pages from @ balakg/balakg.github.io@5550f200d7d9effb57de52221756bccbdefbed0f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,15 +405,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "news-metaseg-just-won-the-best-paper-award-at-miccai-we-offer-a-completely-new-way-to-perform-medical-image-segmentation-using-inrs-with-90-fewer-parameters-than-standard-models",
+          section: "News",},{id: "news-wassdiff",
+          title: 'WassDiff',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-metaseg-just-won-the-best-paper-award-at-miccai-we-offer-a-completely-new-way-to-perform-medical-image-segmentation-using-inrs-with-90-fewer-parameters-than-standard-models",
           title: 'MetaSeg just won the Best Paper Award at MICCAI! We offer a completely...',
           description: "",
           section: "News",},{id: "projects-efficient-signal-representations",
