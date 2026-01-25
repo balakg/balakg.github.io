265e02d2253db2e956b5e05662a797c57e0c6db0
balakg <balakg@mit.edu>
2026-01-25 21:12:04 +0000

    Deploying to gh-pages from @ balakg/balakg.github.io@67793e4ae0138ec9a341540451408399db26148b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,8 +421,8 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-reliable-ai",
+          title: 'Reliable AI',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
